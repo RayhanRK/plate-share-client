e179a741d3abb8b52b5649fb14acc6bd01eecaa3
RayhanRK <m.rayhankhan01@gmail.com>
2025-11-11 01:37:55 +0600

    home page 3 section Added

--- src/components/HomePage/FeaturedFoods.jsx
+++ src/components/HomePage/FeaturedFoods.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Card from '../Card/Card';
+const FeaturedFoods = ({ featuredFoods }) => {
+  console.log(featuredFoods);
 
-const FeaturedFoods = () => {
-    return (
-        <div>
-          from  FeaturedFoods section
+  useEffect(() => {
+    AOS.init({ duration: 1000, easing: 'ease-in-out', once: true });
+  }, []);
+  return (
+    <section className="bg-green-100">
+      <div data-aos="fade-up" className="px-4 mx-auto container py-20 ">
+        {/* Heading */}
+        <div className="mb-15 flex flex-col items-center">
+          <h2 className=" text-3xl text-accent md:text-5xl font-semibold mb-4">
+            Featured <span className="text-primary">Foods</span>
+          </h2>
+          <p className=" text-gray-700 italic">
+           {/* This Week’s Most Kindhearted Food Shares */}
+           This Week’s Most Caring Food Shares
+
+          </p>
         </div>
-    );
+
+        {/* Foods */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-20 mb-10">
+          {featuredFoods?.map(food => (
+            <Card key={food._id} food={food} />
+          ))}
+        </div>
+        {/* Button */}
+        <div className="flex justify-center">
+          <Link
+            to="/available-foods"
+            className="btn btn-primary px-6 text-white text-center hover:bg-green-500 rounded-md border-0"
+          >
+            Show All
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedFoods;
--- src/components/HomePage/HowItWorks.jsx
+++ src/components/HomePage/HowItWorks.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
-
+import { FaUtensils, FaSearch, FaHandHoldingHeart } from "react-icons/fa";
+import { PiBowlFoodBold } from "react-icons/pi";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 const HowItWorks = () => {
-    return (
-        <div>
-            HowItWorks section
+  useEffect(() => {
+    AOS.init({ duration: 1000, easing: "ease-in-out", once: true });
+  }, []);
+  const steps = [
+    {
+      id: 1,
+      icon: <PiBowlFoodBold className="text-primary size-15 lg:size-20" />,
+      title: "Share Your Food",
+      description:
+        "Have extra food? Post its name, description, quantity, and pickup location, and help someone in your community today.",
+    },
+    {
+      id: 2,
+      icon: <FaSearch className="text-primary size-15 lg:size-20" />,
+      title: "Find Food",
+      description:
+        "Browse available food in your area, see photos and locations, and connect with generous community members.",
+    },
+    {
+      id: 3,
+      icon: <FaHandHoldingHeart className="text-primary size-15 lg:size-20" />,
+      title: "Collect Food",
+      description:
+        "Found a meal you need? Contact the donor, pick it up, and enjoy your food. Let's reduce food waste together.",
+    },
+  ];
+
+  return (
+    <section className="py-10 md:py-20 bg-green-50">
+      <div
+        data-aos="fade-up"
+        data-aos-duration="2000"
+        className="container mx-auto px-4 text-center"
+      >
+        {/* Heading */}
+        <div className="mb-15 flex flex-col items-center">
+          <h2 className=" text-3xl text-accent md:text-5xl font-semibold mb-4">
+            How It <span className="text-primary">Works</span>
+          </h2>
+          <p className="text-gray-700 max-w-xl mx-auto">
+            Follow simple steps with PlateShare to share and receive meals.
+            Contribute your community for reducing food wastage every day.
+          </p>
         </div>
-    );
+
+        <div className="grid gap-15 md:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+          {steps.map((step) => (
+            <div
+              key={step.id}
+              className="card bg-white shadow-lg hover:shadow-xl transition duration-300 rounded-xl p-6  flex flex-col items-center text-center hover:scale-105"
+            >
+              <div className="mb-8">{step.icon}</div>
+              <h3 className="text-2xl font-semibold mb-2">{step.title}</h3>
+              <p className="text-gray-700">{step.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HowItWorks;
